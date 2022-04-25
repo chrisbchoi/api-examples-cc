6a452b9a653cf6af770c55d0df03b221cc0e500d
Chris Choi <chrisb.choi@gmail.com>
2022-04-25 19:53:11 +1000

    all

---
+++ README.md
@@ -0,0 +1,7 @@
+
+**Start local docker
+**Run OPEN API editor
+
+`javascript
+$ ./scripts/open_swagger_ui.sh
+`
---
+++ src/utils/server.ts
@@ -0,0 +1,73 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+// import {OpenApiValidator} from 'express-openapi-validator' // if version 3.*
+import * as OpenApiValidator from 'express-openapi-validator'
+import {Express} from 'express-serve-static-core'
+import morgan from 'morgan'
+import morganBody from 'morgan-body'
+import {connector, summarise} from 'swagger-routes-express'
+import YAML from 'yamljs'
+ 
+import * as api from '@exmpl/api/controllers'
+// import { expressDevLogger } from './express_dev_logger'
+import config from '@exmpl/config'
+
+import {expressDevLogger} from '@exmpl/utils/express_dev_logger'
+
+export async function createServer(): Promise<Express> {
+  const yamlSpecFile = './config/openapi.yml'
+  const apiDefinition = YAML.load(yamlSpecFile)
+  const apiSummary = summarise(apiDefinition)
+  console.info(apiSummary)
+ 
+  const server = express()
+  // here we can intialize body/cookies parsers, connect logger, for example morgan
+ 
+  server.use(bodyParser.json())
+
+  if (config.morganLogger) {
+    server.use(morgan(':method :url :status :response-time ms - :res[content-length]'))  
+  }
+  if (config.morganBodyLogger) {
+    morganBody(server)
+  }
+
+  if (config.exmplDevLogger) {
+    server.use(expressDevLogger)
+  }
+
+  // setup API validator
+  const validatorOptions = {
+    coerceTypes: true,
+    apiSpec: yamlSpecFile,
+    validateRequests: true,
+    validateResponses: true
+  }
+//   await new OpenApiValidator(validatorOptions).install(server) // if version 3.*
+  server.use(OpenApiValidator.middleware(validatorOptions))
+  
+  // error customization, if request is invalid
+  server.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(err.status).json({
+      error: {
+        type: 'request_validation',
+        message: err.message,
+        errors: err.errors
+      }
+    })
+  })
+ 
+  const connect = connector(api, apiDefinition, {
+    onCreateRoute: (method: string, descriptor: any[]) => {
+      descriptor.shift()
+      console.log(`${method}: ${descriptor.map((d: any) => d.name).join(', ')}`)
+    },
+    security: {
+      bearerAuth: api.auth
+    }
+  })
+
+  connect(server)
+ 
+  return server
+}
